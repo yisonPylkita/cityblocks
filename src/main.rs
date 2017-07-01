ff054ff43d5fe30d9dc2819ed545ea531c2a61f8
Wojciech Bartnik <yisonPylkita@gmail.com>
2017-07-01 12:39:13 +0200

    Now resembles City Blocks (... just a little). Fixes #1

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ fn main() {
     .exit_on_esc(true).build().unwrap();
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
-            clear([1.0; 4], g);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
+            clear([0.0, 1.0, 1.0, 1.0], g);
+            rectangle([0.15, 0.69, 0.35, 1.0],
+                      [220.0, 0.0, 200.0, 100.0],
                       c.transform, g);
         });
     }
